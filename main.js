44e430446475c2ddb1d1973eb4dec8b7d62cbd93
Bercik4200 <118359839+Bercik4200@users.noreply.github.com>
2024-09-11 12:11:53 +0200

    Whole Code

---
+++ main.js
@@ -0,0 +1,29 @@
+function login(){
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+    var location = "";
+    for(var i = 0; i < objUsers.length; i++){
+        if(username == objUsers[i].username
+            && password == objUsers[i].password){
+                window.open ("podstrona1.html", false);
+                alert( username + " zalogowany");
+                return;
+            }
+    }
+    alert("Nieprawidlowy login lub haslo!");
+}
+var objUsers = [
+    {
+        username: "Albert", 
+        password: "asda"
+    },
+    {
+        username: "Krzysztof",
+        password: "kti"
+    },
+    {
+        username: "Adam",
+        password: "oki"
+    }
+
+]
